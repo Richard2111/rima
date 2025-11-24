643aea084101b76baaab1b5c27f0461d287470b8
Richard2111 <richardandrade2111@gmail.com>
2025-11-24 15:16:59 -0400

    mi proyecto2

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const cors = require('cors');
-app.use(cors());
-
 
 const app = express();
 
+app.use(cors());
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
